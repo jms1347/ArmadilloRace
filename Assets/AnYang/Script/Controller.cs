9dd201ab89a2fee62f480f13ed2908dfedf995ec
jms1347 <rroye@naver.com>
2024-02-23 18:58:44 +0900

    d
    d

---
+++ Assets/AnYang/Script/Controller.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("마우스 클릭");
+
+            //if()
+        }
+    }
+}
